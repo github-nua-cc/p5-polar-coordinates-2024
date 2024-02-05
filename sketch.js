65d83aaaea8696d070579866b4560d2dfd7a6d7c
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-02-05 16:26:42 +0000

    cartesian to html

--- sketch.js
+++ sketch.js
@@ -1,3 +1,22 @@
+// Convert a radius and theta to to an x and y object
+// In order to use this function, we can do the following anywhere in the code:
+// const cartesian = polarToCartesian(radius, theta);
+// If we use cartesian.x afterwards, that contains the x coordinate value
+// if we use cartesian.y afterwards, that contains the y coordinate value
+function polarToCartesian(radius, theta) {
+  return { x: radius * cos(theta), y: -radius * sin(theta) };
+}
+
+// Convert an x and y in cartesian to html
+// In order to use this function, we can do the following anywhere in the code:
+// const html = cartesianToHtml(cartesian); -> where cartesian is an object of type {x: xCoordinate, y: yCoordinate}
+// If we use html.x afterwards, that contains the x coordinate value in html form
+// if we use html.y afterwards, that contains the y coordinate value in html form
+function cartesianToHtml(cartesian) {
+  const centre = { x: windowWidth / 2, y: windowHeight / 2 };
+  return { x: centre.x + cartesian.x, y: centre.y + cartesian.y };
+}
+
 function setup() {
   // create a canvas of size windowWidth x windowHeight
   // windowWidth and windowHeight are variables given to us by by p5.js
@@ -14,9 +33,11 @@ function setup() {
   //remove stroke
   noStroke();
 
-  //give a fill
-  fill(color(0, 200, 200))
-
-  // to draw a point in the centre:
-  circle(centre.x, centre.y, 50)
+  // to draw a point in the centre
+  let radius = 0;
+  let theta = 0;
+  let cartesianCoordinates = polarToCartesian(radius, theta);
+  let htmlCoordinates = cartesianToHtml(cartesianCoordinates);
+  fill(color(0, 200, 200));
+  circle(htmlCoordinates.x, htmlCoordinates.y, 50);
 }
