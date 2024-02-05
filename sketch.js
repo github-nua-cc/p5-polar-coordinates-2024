dbd5069546cd8c73d9d9cd9fa5f48a60246b0eb0
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-02-05 16:22:54 +0000

    intro objects

--- sketch.js
+++ sketch.js
@@ -5,4 +5,18 @@ function setup() {
 
   // set up background to greyscale 220
   background(220);
+
+  // centre stores the centre in an object
+  // centre.x gives the horizontal coordinate of the centre
+  // centre.y gives the vertical coordinate of the centre
+  const centre = { x: windowWidth / 2, y: windowHeight / 2 };
+
+  //remove stroke
+  noStroke();
+
+  //give a fill
+  fill(color(0, 200, 200))
+
+  // to draw a point in the centre:
+  circle(centre.x, centre.y, 50)
 }
