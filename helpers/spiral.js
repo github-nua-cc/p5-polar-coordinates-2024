c680cf83fe998854b024bbc46c8ef3bab519c2f6
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-02-06 11:53:15 +0000

    figured out issue with undrawn spiral

--- helpers/spiral.js
+++ helpers/spiral.js
@@ -1,9 +1,64 @@
 let offset = 0;
+const stroke = 20;
 
+/**
+ * Draw a circle with given radius and pitheta, extracting color from the position colorIndex
+ * @param {Number} radius in pixels
+ * @param {Number} piTheta in radians
+ * @param {Number} colorIndex of the color array
+ */
+function drawACircleWithIndex(radius, piTheta, colorIndex) {
+  //get html coordinates for this degree
+  const htmlCoordinates = polarToHtml(radius, piTheta);
+  fill(colors[colorIndex]);
+  circle(htmlCoordinates.x, htmlCoordinates.y, stroke);
+}
+
+/**
+ * Check if we should draw a point at the given radius. We will not draw a point when:
+ * - radius is so small that it would overlapp with the beginning of the spiral
+ * - colorIndex is out of range (we don't have enough colors)
+ * @param {Number} radius in pixels
+ * @param {Number} theta in radians
+ * @param {Number} colorIndex index of colors array
+ * @returns
+ */
+function shouldDrawHere(nextRadius, theta, colorIndex) {
+  //if the next radius at which I am going to draw is bigger than stroke / 2, the point would be drawn before the original spiral has begun
+  if (nextRadius < stroke / 2) return false;
+
+  //if there are no more colors in the array, we would have an error
+  if (colorIndex >= colors.length) return false;
+
+  //if point is fully outside of canvas we will not draw
+  const htmlCoordiantes = polarToHtml(nextRadius, theta);
+  if(htmlCoordiantes.x > canvasSize + stroke || htmlCoordiantes.y > canvasSize + stroke) return false;
+
+  //any other case is fine to draw
+  return true;
+}
+
+function drawPointsAtThisRadiusTheta(radius, piTheta) {
+  //nextRadius is the radius at which we are drawing the next point which will be update throuhgouth the while
+  let nextRadius = radius;
+  //colorIndex is the index color of this spiral, as set in the setup, which will also be update throughout the while
+  let colorIndex = 0;
+
+    while(nextRadius > stroke / 2) {
+    if(shouldDrawHere(nextRadius, piTheta, colorIndex))
+    //draw circle
+    drawACircleWithIndex(nextRadius, piTheta, colorIndex);
+
+    //update radius
+    nextRadius = nextRadius - stroke / 2;
+    colorIndex++;
+    }
+}
+
+/**
+ * function that manages the drawing of all spirals
+ */
 function drawSpirals() {
-  //set stroke
-  let stroke = 20;
-
   //drawing a circle using radians and degrees
   for (let theta = 0; theta < 7 * 360; theta = theta + 1) {
     //change theta to pi style
@@ -12,25 +67,8 @@ function drawSpirals() {
     //get radius
     const radius = radiusForSpiral(piTheta);
 
-    //get html coordinates for this degree
-    const htmlCoordinates = polarToHtml(radius, piTheta + offset);
-    fill(colors[0]);
-    circle(htmlCoordinates.x, htmlCoordinates.y, stroke);
-
-    //populate interior with random colors
-    let nextRadius = radius - stroke / 2;
-    let colorIndex = 1;
-    while (nextRadius > stroke / 2 && colorIndex < colors.length) {
-      const nextCoordinates = polarToHtml(nextRadius, piTheta + offset);
-
-      //fill circle
-      fill(colors[colorIndex]);
-      circle(nextCoordinates.x, nextCoordinates.y, stroke);
-
-      //update radius
-      nextRadius = nextRadius - stroke / 2;
-      colorIndex++;
-    }
+    //draw inner spirals
+    drawPointsAtThisRadiusTheta(radius, piTheta + offset);
   }
 
   offset = offset + 0.04;
