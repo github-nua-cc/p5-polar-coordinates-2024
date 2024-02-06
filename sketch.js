77c6716c6abcccfd67cde761dfe8fa6b8129ea7b
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-02-06 12:13:43 +0000

    refactor more

--- sketch.js
+++ sketch.js
@@ -1,12 +1,6 @@
 //initially canvas size is zero
 let canvasSize = 0;
 
-//the archimedes spiral formula is r = a * theta, where a is a number of our choice
-// we can choose a to be any number, so let's choose something that looks good!
-function radiusForSpiral(theta) {
-  // a is a ratio that has been found empirically
-  return (50 / 2880) * windowWidth * theta;
-}
 
 function setup() {
   noStroke();
