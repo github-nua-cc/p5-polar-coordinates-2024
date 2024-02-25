c7cde6a5b003df6d9781c9729ee7bd5951af0109
Diana0604 <diana.vallverdu@gmail.com>
2024-02-25 15:01:21 +0000

    beautiful navbar coming from top

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,45 @@
+const navbarContainer = document.getElementById("nav-container");
 const navbar = document.getElementById("nav-bar");
+const shapeTitle = document.getElementById("shape-title");
 
-navbar.onmouseleave = () => {
-  navbar.classList.add("navbar-disappear");
+// navbar.onmouseleave = () => {
+//   navbarContainer.classList.remove("appear");
+//   navbarContainer.classList.add("disappear");
+// };
+
+// navbar.onmouseenter = () => {
+//   // setTimeout(() => {
+//     navbarContainer.classList.remove("disappear");
+//     navbarContainer.classList.add("appear");
+//   // });
+// };
+
+// if (shapeTitle) {
+//   shapeTitle.onmouseenter = () => {
+//     shapeTitle.classList.remove("appear");
+//     shapeTitle.classList.add("disappear");
+//   };
+
+//   shapeTitle.onmouseleave = () => {
+//     shapeTitle.classList.remove("disappear");
+//     shapeTitle.classList.add("appear");
+//   };
+// }
+
+navbarContainer.onmouseenter = () => {
+  navbar.classList.remove("disappear");
+  navbar.classList.add("appear");
+  if (shapeTitle) {
+    // shapeTitle.classList.remove("appear");
+    // shapeTitle.classList.add("disappear");
+  }
+};
+
+navbarContainer.onmouseleave = () => {
+  navbar.classList.remove("appear");
+  navbar.classList.add("disappear");
+  if (shapeTitle) {
+    // shapeTitle.classList.remove("disappear");
+    // shapeTitle.classList.add("appear");
+  }
 };
