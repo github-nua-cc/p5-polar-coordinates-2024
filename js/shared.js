988768632fc1171bf405469f2e7630ff3714b9bd
Diana0604 <diana.vallverdu@gmail.com>
2024-02-25 15:16:47 +0000

    generalize navbar appearance styles

--- js/shared.js
+++ js/shared.js
@@ -2,11 +2,11 @@ const navbarContainer = document.getElementById("nav-container");
 const navbar = document.getElementById("nav-bar");
 
 navbarContainer.onmouseenter = () => {
-  navbar.classList.remove("disappear");
-  navbar.classList.add("appear");
+  navbar.classList.remove("disappear-from-top");
+  navbar.classList.add("appear-from-top");
 };
 
 navbarContainer.onmouseleave = () => {
-  navbar.classList.remove("appear");
-  navbar.classList.add("disappear");
+  navbar.classList.remove("appear-from-top");
+  navbar.classList.add("disappear-from-top");
 };
